74fa1014cbaf9a14865dd1e83ad7ba3cc7d134ea
Yash153 <78364754+Yash153@users.noreply.github.com>
2022-01-09 10:45:59 +0530

    Update settings.py

--- IMproject/settings.py
+++ IMproject/settings.py
@@ -129,7 +129,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'demoo132001@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pillow@13'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
